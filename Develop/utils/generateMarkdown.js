41bc1f6be8c5cb3f58bdb8144943f853db438413
kolatham <92490383+kolatham@users.noreply.github.com>
2022-01-08 14:17:02 -0500

    added license badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -6,13 +6,19 @@ switch (license) {
 case "MIT License":
   return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
 case "GNU AGPLv3":
-  return ;
+  return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
 case "Mozilla Public License 2.0":
-  return;
+  return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+case "GNU GPLv3":
+  return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+case "GNU LGPLv3":
+  return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`;
+case"Apache License 2.0":
+  return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+case "Boost Software License 1.0":
+  return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
 
-
-}
-
+  }
 }
 
 
