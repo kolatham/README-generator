ea812e3183e822dee4047709af8180f3d3da4814
kolatham <92490383+kolatham@users.noreply.github.com>
2022-01-08 14:17:51 -0500

    made changes to index

--- Develop/index.js
+++ Develop/index.js
@@ -51,7 +51,7 @@ validate: function (answer) {
     type: "list",
     name:"license",
     message:"Choose a license for your project",
-    choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense', 'none']
+    choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'none']
 },
 
 ];
